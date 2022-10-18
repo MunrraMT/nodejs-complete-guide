f4c21a9e7bbb3bde255caa11a439bfa219267267
André Rodrigues Melgaço dos Santos <dev.armsantos@gmail.com>
2022-10-17 23:10:35 -0400

    Aula - 29. Understanding Requests

--- secao-03/aula-26/app.js
+++ secao-03/aula-26/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-  console.log(request);
+  console.log([request.url, request.method, request.headers]);
   process.exit();
 });
 
