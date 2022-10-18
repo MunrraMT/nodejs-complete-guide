bb16f1d7719a9aaffb29fd5ae8af6a3b966d40fd
André Rodrigues Melgaço dos Santos <dev.armsantos@gmail.com>
2022-10-17 23:20:09 -0400

    Aula - 30. Sending Responses

--- secao-03/aula-26/app.js
+++ secao-03/aula-26/app.js
@@ -2,7 +2,14 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
   console.log([request.url, request.method, request.headers]);
-  process.exit();
+  response.setHeader('Content-Type', 'text/html');
+  response.write(`<html>
+    <head>
+      <title>My first page</title>
+      <body><h1>Hello from my node server!</h1></body>
+    </head>
+  </html>`);
+  response.end();
 });
 
 server.listen(3000);
