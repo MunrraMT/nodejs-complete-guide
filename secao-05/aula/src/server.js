bb1074996a7637b22d689a107863f67a32e74bd1
André Rodrigues Melgaço dos Santos <dev.armsantos@gmail.com>
2022-10-21 00:13:07 -0400

    Aula - 60. Adding Middleware

--- secao-05/aula/src/server.js
+++ secao-05/aula/src/server.js
@@ -1,12 +1,23 @@
+/* eslint-disable */
+
 const http = require('http');
-const routes = require('./routes');
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
+app.use((request, response, next) => {
+  console.log('Middleware 1');
+  next();
+});
+
+app.use((request, response, next) => {
+  console.log('Middleware 2');
+});
+
 // app.listen(3000, () => {
 //   console.log('Server init - http://localhost:3000/');
-// })
+// });
 
 const server = http.createServer(app);
 
