8f26d3a6b367c22da1be4658f1f1f7c18cbd2012
André Rodrigues Melgaço dos Santos <dev.armsantos@gmail.com>
2022-10-24 23:43:33 -0400

    Aula - 67. Adding a 404 Error Page

--- secao-05/aula/src/server.js
+++ secao-05/aula/src/server.js
@@ -11,6 +11,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRouter);
 app.use(shopRouter);
 
+app.use('/', (request, response) => {
+  response.status(404).send('<h1>404</h1>');
+});
+
 app.listen(3000, () => {
   console.log('Server init - http://localhost:3000/');
 });
