00f5d0de3aeb0b2f37f7fdff34636150c4799208
André Rodrigues Melgaço dos Santos <dev.armsantos@gmail.com>
2022-10-26 23:06:44 -0400

    Aula - 75. Serving Files Statically

--- secao-05/aula/src/server.js
+++ secao-05/aula/src/server.js
@@ -1,13 +1,17 @@
 /* eslint-disable */
 
-const app = require('express')();
+const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
+const app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
