3581da326201cd9961b25aa9da3362ed65c09582
André Rodrigues Melgaço dos Santos <dev.armsantos@gmail.com>
2022-10-26 22:49:07 -0400

    Aula - 73. Using a Helper Function for Navigation

--- secao-05/aula/src/routes/admin.js
+++ secao-05/aula/src/routes/admin.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../utils/path');
+
 const router = express.Router();
 
 router.get('/add-product', (request, response) => {
-  response.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  response.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (request, response) => {
--- secao-05/aula/src/routes/shop.js
+++ secao-05/aula/src/routes/shop.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../utils/path');
+
 const router = express.Router();
 
 router.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+  response.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
