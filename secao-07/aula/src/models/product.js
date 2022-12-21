d8ea9dbd611e616e8a0965e357c7f819dfbd5cf9
André Rodrigues Melgaço dos Santos <dev.armsantos@gmail.com>
2022-12-21 01:17:59 -0400

    101. Storing Data in Files Via the Model

--- secao-07/aula/src/models/product.js
+++ secao-07/aula/src/models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const pathToDB = path.join(__dirname, '..', 'data', 'products.json');
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +9,24 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    fs.readFile(pathToDB, (readError, fileContent) => {
+      let products = [];
+
+      if (!readError) {
+        products = JSON.parse(fileContent);
+      }
+
+      products.push(this);
+      fs.writeFile(pathToDB, JSON.stringify(products), (writeError) => {
+        console.log(writeError);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    fs.readFile(pathToDB, (err, fileContent) => {
+      if (err) return [];
+      return JSON.parse(fileContent);
+    });
   }
 };
